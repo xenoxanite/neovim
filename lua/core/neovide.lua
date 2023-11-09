4fe29bde3edc2eb56f98ae38577b4f87207c12b8
xenoxanite <xenoxanite@gmail.com>
2023-11-09 15:57:05 +0600

    updated

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -1,6 +1,14 @@
 if vim.g.neovide then
   vim.o.guifont = "Iosevka nerd font:h14"
-  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.opt.linespace = 3
+  vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_transparency= 0.72
+  vim.g.neovide_transparency = 0.7
+  vim.g.neovide_refresh_rate = 75
+  vim.g.neovide_no_idle = true
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_vfx_particle_density = 20
+  vim.g.neovide_scroll_animation_length = 0.3
+  vim.g.neovide_cursor_vfx_particle_lifetime = 0.5
 end
