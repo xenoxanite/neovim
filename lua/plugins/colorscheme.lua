6e7d5f3f52be1f518e3adc30cd57304d7ebca5b5
xenoxanite <xenoxanite@gmail.com>
2023-11-13 17:53:46 +0600

    updated colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,31 @@
 local nightfox = {
-	"EdenEast/nightfox.nvim",
-	config = function()
-		require("nightfox").setup({
-			options = {
+  "EdenEast/nightfox.nvim",
+  config = function()
+    require("nightfox").setup({
+      options = {
 
-				transparent = true,
-				styles = {
-					comments = "italic",
-					keywords = "bold",
-					types = "italic,bold",
-				},
-			},
-		})
-		vim.cmd("colorscheme nightfox")
-	end,
+        transparent = true,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      },
+    })
+    vim.cmd("colorscheme nightfox")
+  end,
 }
 
-return nightfox
+local pywal = {
+  "uZer/pywal16.nvim",
+  as = "pywal16",
+  config = function()
+    local pywal16 = require("pywal16")
+
+    pywal16.setup()
+  end,
+  priority = 1000, -- recommended to ensure the colorscheme
+  -- is loaded before other plugins
+}
+
+return pywal
