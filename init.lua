90cad6eae7a52309bf024c37059832de2a882db3
samiulbasirfahim <xenoxanite@gmail.com>
2023-12-30 16:44:56 +0600

    norg plugin installed

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require("core.options")
 require("core.keymaps")
 require("core.neovide")
-require("core.theme")
 require(".lazy")
-
+require("core.theme")
--- lua/plugins/cphelper.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "p00f/cphelper.nvim",
-  dependencies = "nvim-lua/plenary.nvim",
-  config = function()
-    local home = os.getenv("HOME")
-    vim.g["cph#dir"] = home .. "/Programming/competetive-programming"
-    vim.g["cph#lang"] = "cpp"
-  end,
-}
