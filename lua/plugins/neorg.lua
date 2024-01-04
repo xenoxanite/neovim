460611faa6734b01af0916cba21bb9699e99b8f7
samiulbasirfahim <xenoxanite@gmail.com>
2024-01-04 16:36:50 +0600

    updated a banch of stuff

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,34 +1,26 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  lazy = false,
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {},
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-            default_workspace = "notes",
-          },
-        },
-      },
-    })
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							notes = "~/notes",
+						},
+						default_workspace = "notes",
+					},
+				},
+			},
+		})
 
-    vim.wo.foldlevel = 99
-    vim.wo.conceallevel = 2
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set(
-      "n",
-      "<leader>nn",
-      "<cmd>Neorg keybind all core.dirman.new.note<CR>",
-      { desc = "Toggle file explorer" }
-    )
-  end,
+		vim.wo.foldlevel = 99
+		vim.wo.conceallevel = 2
+		-- set keymaps
+	end,
 }
--- lua/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"folke/trouble.nvim",
-	event = "BufReadPost",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.keymap.set("n", "<leader>t", "<cmd>Trouble<CR>")
-	end,
-}
